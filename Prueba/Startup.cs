5a7d239c45604d26ef484d28640317fd72db074d
Carmen <carmela25.ccg@gmail.com>
2021-03-07 17:07:35 +0100

    swagger

--- Prueba/Startup.cs
+++ Prueba/Startup.cs
@@ -4,11 +4,13 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using PruebaBL.Contracts;
 using PruebaBL.Implementation;
 using PruebaDAL.Models;
 using PruebaDAL.Repositories.Contracts;
 using PruebaDAL.Repositories.Implementations;
+using System;
 
 namespace Prueba
 {
@@ -25,6 +27,7 @@ namespace Prueba
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            AddSwagger(services);
 
             //CORS
             services.AddCors(options =>
@@ -45,6 +48,27 @@ namespace Prueba
             services.AddScoped<IVentaRepositorio, VentaRepositorio>();
         }
 
+        private void AddSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                var groupName = "v1";
+
+                options.SwaggerDoc(groupName, new OpenApiInfo
+                {
+                    Title = $"Foo {groupName}",
+                    Version = groupName,
+                    Description = "Foo API",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Foo Company",
+                        Email = string.Empty,
+                        Url = new Uri("https://foo.com/"),
+                    }
+                });
+            });
+        }
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -54,6 +78,12 @@ namespace Prueba
             }
 
             app.UseHttpsRedirection();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Foo API V1");
+            });
 
             app.UseRouting();
 
